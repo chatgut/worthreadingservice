6b54a3ae0dec3c5814c92d7754bfe2eccd610e69
Christofer Nyström <christofer.nystrom@iths.se>
2023-05-09 01:55:36 +0200

    Slight refactor

--- src/main/java/com/example/worthreadingservice/WorthReadingServiceApplication.java
+++ src/main/java/com/example/worthreadingservice/WorthReadingServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WorthreadingserviceApplication {
+public class WorthReadingServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(WorthreadingserviceApplication.class, args);
+        SpringApplication.run(WorthReadingServiceApplication.class, args);
     }
 
 }
--- src/main/java/com/example/worthreadingservice/service/UserDto.java
+++
@@ -1,4 +0,0 @@
-package com.example.worthreadingservice.service;
-
-public class UserDto {
-}
